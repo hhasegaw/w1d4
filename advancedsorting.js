3dff5c2a9f33c1827d9b415b29aa5024b5f215bb
hanna hasegawa <noemail@noemail.com>
2018-08-30 19:51:14 -0400

    1 task completed

--- advancedsorting.js
+++ advancedsorting.js
@@ -1,11 +1,10 @@
-var students = [
-   
+var students = [ 
     { id: 1, name: "bruce",    age: 40 },
     { id: 2, name: "zoidberg", age: 22 },
     { id: 3, name: "alex",     age: 22 },
     { id: 4, name: "alex",     age: 30 }
   ];
-  
+
   var result = students.sort (function(a, b){
     //return a.name > b.name; 
     if (a.name === b.name) {
